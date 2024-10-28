9c22ee6bc2a4c0db7a751ebed42d9fb65ba1993d
integralpir <Orlov-RP@yandex.ru>
2024-10-28 21:44:57 +0500

    [Пара №4] основная информация, примеры кода

---
+++ src/lesson4/examples/ArrayClient.java
@@ -0,0 +1,42 @@
+package lesson4.examples;
+
+import java.util.Random;
+
+public class ArrayClient {
+    public static void main(String[] args) {
+        String separator = "__________________________________________________________";
+        Random random = new Random();
+        int size = 100;
+        CommonArray commonArray = new CommonArray(size);
+        OrderedArray orderedArray = new OrderedArray(size);
+
+        for (int i = 0; i < size; i++) { // заполнение массива
+            boolean insertResultCommonArray = false;
+            boolean insertResultOrderedArray = false;
+
+            while (!insertResultCommonArray) {
+                insertResultCommonArray = commonArray.insert(random.nextLong(1000));
+            }
+
+            while (!insertResultOrderedArray) {
+                insertResultOrderedArray = orderedArray.insert(random.nextLong(1000));
+            }
+        }
+
+        long searchValue = random.nextLong(1000);
+
+        if (commonArray.contains(searchValue)) {
+            System.out.println("Значение было найдено. " + searchValue);
+        } else {
+            System.out.println("Не удалось найти значение. " + searchValue);
+        }
+
+        System.out.println(separator);
+
+        if (orderedArray.contains(searchValue)) {
+            System.out.println("Значение было найдено. " + searchValue);
+        } else {
+            System.out.println("Не удалось найти значение. " + searchValue);
+        }
+    }
+}
---
+++ src/lesson4/examples/ParentLongArray.java
@@ -0,0 +1,83 @@
+package lesson4.examples;
+
+public abstract class ParentLongArray implements ArrayInterface {
+    protected final long[] array;
+    protected int nElems;
+
+    public ParentLongArray(int size) {
+        this.array = new long[size];
+        this.nElems = 0;
+    }
+
+    @Override
+    public boolean insert(long value) {
+        for (int i = 0; i < nElems; i++) {
+            if (array[i] == value) {
+                return false;
+            }
+        }
+
+        array[nElems] = value;
+        nElems++;
+        return true;
+    }
+
+    @Override
+    public boolean delete(long value) {
+        int i;
+        for (i = 0; i < this.nElems; i++) {
+            if (array[i] == value) {
+                break;
+            }
+        }
+
+        if (i == nElems - 1) {
+            return false;
+        } else {
+            for (int j = i; j < nElems - 1; j++) {
+                array[j] = array[j + 1];
+            }
+            nElems--;
+            return true;
+        }
+    }
+
+    @Override
+    public long getMax() {
+        long maxValue = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nElems; i++) {
+            if (array[i] > maxValue) {
+                maxValue = array[i];
+            }
+        }
+
+        return maxValue;
+    }
+
+    @Override
+    public long getMin() {
+        long minValue = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nElems; i++) {
+            if (array[i] < minValue) {
+                minValue = array[i];
+            }
+        }
+
+        return minValue;
+    }
+
+    @Override
+    public void display() {
+        for (int i = 0; i < nElems; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    @Override
+    public int getSize() {
+        return this.nElems;
+    }
+}
