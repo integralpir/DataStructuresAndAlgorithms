b69514c029481128ddd5dbc8485442edf9fe06f8
Роман Орлов <Orlov-RP@yandex.ru>
2024-11-25 23:59:05 +0500

    [Lesson 6] стек, очередь, приоритетная очередь

---
+++ src/lesson6/examples/stack/StackImpl.java
@@ -0,0 +1,34 @@
+package lesson6.examples.stack;
+
+public class StackImpl {
+
+    private int maxSize;
+    private char[] stackArray;
+    private int top;
+
+    public StackImpl(int s) {
+        maxSize = s;
+        stackArray = new char[maxSize];
+        top = -1;
+    }
+
+    public void push(char j) {
+        stackArray[++top] = j;
+    }
+
+    public char pop() {
+        return stackArray[top--];
+    }
+
+    public char peek() {
+        return stackArray[top];
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public boolean isFull() {
+        return top == maxSize-1;
+    }
+}
